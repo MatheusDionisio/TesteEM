28bb16be4b9f6ae3a7c827d3d5b559aae0d4ab8c
Matheus Dionisio <mdsdionisio@gmail.com>
2021-03-12 10:29:28 -0300

    Matheus 1.0

---
+++ Aluno/Aluno.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Aluno
+{
+    public enum EnumeradorSexo
+    {
+        Masculino=0,
+        Feminino=1
+    }
+    public class Aluno
+    {
+        private int matricula;
+        private int Matricula
+        {
+            get { return matricula; }
+            set {
+                if (Convert.ToString(value).Length > 9 || Convert.ToString(value).Length == 0)
+                    throw new FormatException();
+                matricula = value;
+            }
+        }
+        private string nome;
+        private string Nome
+        {
+            get { return nome; }
+            set 
+            {
+                if (value.Length > 100 || value.Length <= 0)
+                    throw new FormatException();
+                nome = value;
+            }
+        }
+        private string cpf;
+        private string Cpf
+        {
+            get { return cpf; }
+            set
+            {
+                cpf = value;
+            }
+        }
+        private DateTime nascimento;
+        private DateTime Nascimento
+        {
+            get { return nascimento; }
+            set
+            {
+                nascimento = value;
+            }
+        }
+        private EnumeradorSexo sexo;
+        private EnumeradorSexo Sexo
+        {
+            get { return sexo; }
+            set
+            {
+                sexo = value;
+            }
+        }
+        public Aluno(int matriculaIn, string nomeIn, string cpfIn, DateTime nascimentoIn,EnumeradorSexo sexoIn)
+        {
+            Matricula = matriculaIn;
+            Nome = nomeIn;
+            Cpf = cpfIn;
+            Nascimento = nascimentoIn;
+            Sexo = sexoIn;
+
+        }
+        public override string ToString()
+        {
+            return $"Aluno\nMatricula: {matricula}\nNome: {nome}\nCPF: {cpf}\nNascimento{nascimento}\nSexo: {sexo}";
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is Aluno aluno && aluno.matricula == matricula;
+        }
+    }
+}
