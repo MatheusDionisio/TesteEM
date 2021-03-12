d6c45889189c85363d7afb3b441c1717d598a1c7
ziinahzoor <rafaelxsantosx@hotmail.com>
2021-03-12 11:26:06 -0300

    v3.0

--- Aluno/Aluno.cs
+++ Aluno/Aluno.cs
@@ -78,15 +78,17 @@ namespace Aluno
         {
             return Convert.ToString(matricula);
         }
-<<<<<<< HEAD
         public bool AlunoEhTop()
         {
             return nome.Equals("Matheus");
-=======
+        }
         public bool AlunoEhLindo()
         {
             return nome.Equals("Rafael");
->>>>>>> 2623b3a87c13a1ad90b0d4f2b3149490a7ea773d
+        }
+        public bool EhVerdade()
+        {
+            return true;
         }
     }
 }
